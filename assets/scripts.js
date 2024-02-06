8ae715f496762f84b180eb8f1893caca87a56f94
laukstl <laurent@kstl.fr>
2024-02-06 09:55:49 +0100

    Retrait de Splide. Ajout de Swiper. Retrait dee Bootstrap. Retrait d'Idotope. Ajout manuel de filtrage Gal

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,27 +1,66 @@
-new Splide('.splide', {
-  type: 'loop',
-  autoplay: true,
-  interval: 5000,
-  pauseOnHover: true,
-  pauseOnFocus: true,
-  lazyLoad: true
-}).mount()
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable @typescript-eslint/indent */
+/* eslint-disable @typescript-eslint/semi */
+// Init Swipper
+const $lgSwiper = document.getElementById('lg-swipper')
+const swiper = new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
 
-lightGallery(document.getElementById('lightgallery'), {
-  plugins: [lgThumbnail, lgAutoplay],
-  // plugins: [lgZoom, lgThumbnail, lgShare, lgAutoplay, lgComment],
-  licenseKey: '0000-0000-000-0000',
-  speed: 500,
-  animateThumb: false,
-  zoomFromOrigin: false,
-  allowMediaOverlap: true,
-  toggleThumb: true,
-  download: false,
-  mobileSettings: {
-    controls: false,
-    showCloseIcon: false,
-    download: false,
-    rotate: false,
-    infiniteZoom: true
+  // Init lightGallery ince swiper is initilized
+  on: {
+    init: function () {
+      if ($lgSwiper !== null) {
+        const lg = lightGallery($lgSwiper)
+
+        // Before closing lightGallery, make sure swiper slideis aligned with the lightGallery active slide
+        $lgSwiper.addEventListener('lgBeforeClose', () => {
+          swiper.slideTo(lg.index, 0)
+        })
+      }
+    }
+  },
+
+  pagination: {
+    el: '.swiper-pagination'
   }
+})
+
+// Init Lightgallery
+const lgElement = document.getElementById('lightgallery')
+if (lgElement !== null) {
+  lightGallery(lgElement, {
+    // plugins: [lgZoom, lgThumbnail, lgShare, lgAutoplay, lgComment],
+    licenseKey: '0000-0000-000-0000',
+    speed: 500,
+    // allowMediaOverlap: true,
+    download: false,
+    mobileSettings: {
+      controls: false,
+      showCloseIcon: false,
+      download: false
+    }
+  })
+}
+
+// Init nav_Gallery
+document.addEventListener('DOMContentLoaded', () => {
+    const galButtons = document.querySelectorAll('#gallery button');
+    const galPictures = document.querySelectorAll('#gallery a');
+
+    galButtons.forEach((buttons) => {
+        buttons.addEventListener('mouseup', () => {
+            const filter = buttons.getAttribute('data-filter');
+            galPictures.forEach((picture) => {
+                const pictAttr = picture.getAttribute('data-tag');
+                if (filter === 'tous' || pictAttr === filter) {
+                    picture.style.display = 'block';
+                } else {
+                    picture.style.display = 'none';
+                }
+            })
+        })
+    })
 })
