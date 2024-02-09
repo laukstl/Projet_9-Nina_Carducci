361de6215df9bbd4774a95a375c120eaac9ad7ec
laukstl <laurent@kstl.fr>
2024-02-09 23:17:32 +0100

    Ajustements divers

--- assets/scripts.js
+++ assets/scripts.js
@@ -20,12 +20,17 @@ const swiper = new Swiper('.swiper', {
     }
   },
 
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    enabled: false
+  },
+
   loop: true,
 
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false
-  },
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false
+  // },
 
   pagination: {
     el: '.swiper-pagination',
